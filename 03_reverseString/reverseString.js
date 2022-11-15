93b08d4f913bc5d339dfd8027396efbfde4c9bd5
“SonathanJim” <sonathanjim@gmail.com>
2022-11-15 15:42:14 +0000

    complete javascript-exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(givenString) {
+    
+    let string = '';
 
+    for (let i = givenString.length - 1; i>=0; i--) {
+        string += givenString[i];
+    }
+
+    return string;
 };
 
 // Do not edit below this line
