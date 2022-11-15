5196f785f620184b73feb80943993c2c16099e3a
“SonathanJim” <sonathanjim@gmail.com>
2022-11-15 12:40:47 +0000

    Complete javascript-exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+//repeatString('hey', 3) // returns 'heyheyhey'
+//Create a variable to hold the string you're going to return, 
+//create a loop that repeats the given number of times and 
+//add the given string to the result on each loop.
 
+const repeatString = function(givenString, num) {
+    let string = '';
+    
+    if (num <0) {return "ERROR"}
+    
+    {let i=1;
+        while (num>=i) {
+            string += `${givenString}`;
+            i++
+        };
+    return string;}
 };
 
 // Do not edit below this line
