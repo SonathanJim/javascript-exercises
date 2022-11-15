df97039983b3ec1c85568a14ef81e49ead493355
“SonathanJim” <sonathanjim@gmail.com>
2022-11-15 23:23:09 +0000

    complete javascript-xercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let fars = +num;
+  let cels = ((fars - 32) * 5 / 9);
+  let celsRounded = Math.round(cels * 10) / 10;
+  return celsRounded
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let cels = +num;
+  let fars = ((cels * 9 / 5) + 32);
+  let farsRounded = Math.round(fars * 10) / 10;
+  return farsRounded
 };
 
 // Do not edit below this line
