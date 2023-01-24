eed244fd778b895aa6085c7fdbc91e77e3c6b885
“SonathanJim” <sonathanjim@gmail.com>
2023-01-24 12:41:18 +0000

    complete 09_palindromes test exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //declare variable for punctuation and whitespace regexp
+    const regex = /[!@€#£$%^&*()\-_=+[{}\];:'"\\|,<.>\/?`~§± ]/g;
+    //remove whitespace and punctuation
+    const palTestString = string.replace(regex, '');
+    //reverse string
+    const reverseString = function (string) {
+        const revArray = [];
+        const length = string.length - 1;
+        for(let i = length; i >= 0; i--) {
+            revArray.push(string[i]);
+        }
+        return revArray.join('');
+    }
 
+    //check if palindrome ie. does string === stringBackwards
+    if ((palTestString.toLowerCase()) === (reverseString(palTestString).toLowerCase())){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
