bea4bb7cd6ff89b280ca203880c399f4b84e1883
“SonathanJim” <sonathanjim@gmail.com>
2023-01-24 13:32:00 +0000

    complete 10_fibonacci test exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (Math.sign(index) === -1) {
+        return "OOPS"
+    } else {
+        const goldenRatio = 1.618034;
+        const number = ((goldenRatio**index) - ((1-goldenRatio)**index))/Math.sqrt(5);
+        return Math.round(number);
+    }
 };
 
 // Do not edit below this line
