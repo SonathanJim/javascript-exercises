f429c8639975b1681062682136317778d516d80a
“SonathanJim” <sonathanjim@gmail.com>
2023-01-24 23:42:00 +0000

    complete 12_findTheOldest test exercise
    NB. require further learning on array functions. especiallr reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.map(object => (
+        array.map(object => Object.keys(object))
+        .reduce((objectA, objectB) => (objectB.forEach(propertyZ => objectA.includes(propertyZ) || objectA.push(propertyZ)), objectA))
+        .forEach(propertyY => (object[propertyY] = object.hasOwnProperty(propertyY) ? object[propertyY] : new Date().getFullYear())), object)
+    );
+    array.sort(function(personA, personB) {
+        const lastPerson = personA.yearOfDeath - personA.yearOfBirth;
+        const nextPerson = personB.yearOfDeath - personB.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    });
+    return array[0];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
